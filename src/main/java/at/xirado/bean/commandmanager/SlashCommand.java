bee86f348da9956fa78aae7ef6adfa3c74ce72aa
Xirado <bquickzz0@gmail.com>
2021-03-29 11:29:30 +0200

    Added preparation for slash commands

---
+++ src/main/java/at/xirado/bean/commandmanager/SlashCommand.java
@@ -0,0 +1,99 @@
+package at.xirado.bean.commandmanager;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.requests.restaction.CommandUpdateAction;
+import net.dv8tion.jda.api.utils.data.DataArray;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class SlashCommand
+{
+
+    private CommandUpdateAction.CommandData commandData;
+    private List<Permission> neededUserPermissions;
+    private List<Permission> neededBotPermissions;
+    private boolean isGlobal;
+    private List<Long> enabledGuilds;
+
+    public String getCommandName()
+    {
+        return (String) this.commandData.toData().get("name");
+    }
+
+    public String getCommandDescription()
+    {
+        return (String) this.commandData.toData().get("description");
+    }
+
+    public DataArray getOptions()
+    {
+        return (DataArray) this.commandData.toData().get("options");
+    }
+
+
+    public CommandUpdateAction.CommandData getCommandData()
+    {
+        return commandData;
+    }
+
+    public void setCommandData(CommandUpdateAction.CommandData commandData)
+    {
+        this.commandData = commandData;
+    }
+
+    public List<Permission> getNeededUserPermissions()
+    {
+        return neededUserPermissions;
+    }
+
+    public void setNeededUserPermissions(List<Permission> neededUserPermissions)
+    {
+        this.neededUserPermissions = neededUserPermissions;
+    }
+
+    public List<Permission> getNeededBotPermissions()
+    {
+        return neededBotPermissions;
+    }
+
+    public void setNeededBotPermissions(List<Permission> neededBotPermissions)
+    {
+        this.neededBotPermissions = neededBotPermissions;
+    }
+
+    public boolean isGlobal()
+    {
+        return isGlobal;
+    }
+
+    public void setGlobal(boolean global)
+    {
+        isGlobal = global;
+    }
+
+    public List<Long> getEnabledGuilds()
+    {
+        return enabledGuilds;
+    }
+
+    public void setEnabledGuilds(List<Long> enabledGuilds)
+    {
+        this.enabledGuilds = enabledGuilds;
+    }
+
+
+
+    public SlashCommand()
+    {
+        this.commandData = null;
+        this.isGlobal = true;
+        this.enabledGuilds = new ArrayList<>();
+    }
+
+    public abstract void executeCommand(@NotNull SlashCommandEvent event, @NotNull Member sender, @NotNull CommandContext context);
+
+}
--- src/main/java/at/xirado/bean/commandmanager/SlashCommandManager.java
+++ src/main/java/at/xirado/bean/commandmanager/SlashCommandManager.java
@@ -1,10 +1,127 @@
 package at.xirado.bean.commandmanager;
 
 
+import at.xirado.bean.main.DiscordBot;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Command;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.requests.restaction.CommandCreateAction;
+import net.dv8tion.jda.api.requests.restaction.CommandUpdateAction;
+import net.dv8tion.jda.api.utils.data.DataObject;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Commandmanager for slash-commands
  */
 public class SlashCommandManager {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SlashCommandManager.class);
+
+    public final List<SlashCommand> registeredCommands;
+    public final ConcurrentHashMap<Long, List<SlashCommand>> registeredGuildCommands;
+
+    public SlashCommandManager()
+    {
+        registeredCommands = Collections.synchronizedList(new ArrayList<>());
+        registeredGuildCommands = new ConcurrentHashMap<>();
+    }
+
+
+    public void registerAllCommands()
+    {
+
+    }
+
+    private void registerCommand(SlashCommand command)
+    {
+        if(!command.isGlobal())
+        {
+            if(command.getEnabledGuilds() == null) return;
+            if(command.getEnabledGuilds().isEmpty()) return;
+            for(Long guildID : command.getEnabledGuilds())
+            {
+                Guild guild = DiscordBot.getInstance().jda.getGuildById(guildID);
+                if(guild == null) continue;
+                CommandCreateAction commandCreateAction = guild.upsertCommand(command.getCommandName(), command.getCommandDescription());
+                for(Object data : command.getOptions())
+                {
+                    CommandUpdateAction.OptionData optionData = (CommandUpdateAction.OptionData) data;
+                    DataObject dataObject = optionData.toData();
+                    commandCreateAction.addOption((String) dataObject.get("name"), (String) dataObject.get("description"), Command.OptionType.fromKey((int) dataObject.get("type")));
+                }
+                commandCreateAction.queue(
+                        s ->
+                        {
+                            List<SlashCommand> guildCommands = registeredGuildCommands.containsKey(guildID) ? registeredGuildCommands.get(guildID) : new ArrayList<>();
+                            guildCommands.add(command);
+                            registeredGuildCommands.put(guildID, guildCommands);
+                            LOGGER.info("Registered command "+command.getCommandName()+" for guild "+guild.getName());
+                        }
+                );
+            }
+            return;
+        }
+        registeredCommands.add(command);
+    }
+
+
+    public void handleSlashCommand(@NotNull SlashCommandEvent event, @NotNull Member member)
+    {
+        Runnable r = () ->
+        {
+            try
+            {
+                if(event.getGuild() == null) return;
+                for(SlashCommand cmd : registeredCommands)
+                {
+                    if(cmd == null) continue;
+                    if(cmd.getCommandName() == null) continue;
+                    if(cmd.getCommandName().equalsIgnoreCase(event.getName()))
+                    {
+                        event.acknowledge(true);
+                        List<Permission> neededPermissions = cmd.getNeededUserPermissions();
+                        List<Permission> neededBotPermissions = cmd.getNeededBotPermissions();
+                        if(neededPermissions != null)
+                        {
+                            for(Permission permission : neededPermissions)
+                            {
+                                if(!member.hasPermission(permission))
+                                {
+                                    event.reply("You don't have permission to do this!").setEphemeral(true).queue();
+                                    return;
+                                }
+                            }
+                        }
+
+                        if(neededBotPermissions != null)
+                        {
+                            for(Permission permission : neededBotPermissions)
+                            {
+                                if(!event.getGuild().getSelfMember().hasPermission(permission))
+                                {
+                                    event.reply("I don't have the required permission to do this.").setEphemeral(true).queue();
+                                    return;
+                                }
+                            }
+                        }
+                        cmd.executeCommand(event, member, new CommandContext(event));
+                    }
+                }
+            }catch (Exception e)
+            {
+                LOGGER.error("Could not execute slash-command", e);
+            }
+        };
+        DiscordBot.getInstance().scheduledExecutorService.submit(r);
+    }
 
 }
