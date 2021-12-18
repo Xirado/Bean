da728455a4544f313e214bf2c5d0b05bb1765567
Marcel Korzonek <marcel.korzonek@htlstp.at>
2021-12-18 17:59:36 +0100

    Fix stop command DJ logic

--- src/main/java/at/xirado/bean/command/slashcommands/music/StopCommand.java
+++ src/main/java/at/xirado/bean/command/slashcommands/music/StopCommand.java
@@ -1,10 +1,15 @@
 package at.xirado.bean.command.slashcommands.music;
 
+import at.xirado.bean.Bean;
 import at.xirado.bean.command.CommandFlag;
 import at.xirado.bean.command.SlashCommand;
 import at.xirado.bean.command.SlashCommandContext;
 import at.xirado.bean.misc.EmbedUtil;
+import at.xirado.bean.misc.objects.TrackInfo;
+import at.xirado.bean.music.GuildAudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
@@ -12,12 +17,16 @@ import net.dv8tion.jda.api.managers.AudioManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StopCommand extends SlashCommand
 {
     public StopCommand()
     {
         setCommandData(new CommandData("stop", "Disconnects the bot and clears the queue."));
-        addCommandFlags(CommandFlag.DJ_ONLY, CommandFlag.MUST_BE_IN_SAME_VC, CommandFlag.MUST_BE_IN_VC);
+        addCommandFlags(CommandFlag.MUST_BE_IN_SAME_VC, CommandFlag.MUST_BE_IN_VC);
     }
 
     @Override
@@ -29,8 +38,49 @@ public class StopCommand extends SlashCommand
             event.replyEmbeds(EmbedUtil.warningEmbed("I am not connected to a voice channel!")).queue();
             return;
         }
+        GuildAudioPlayer player = Bean.getInstance().getAudioManager().getAudioPlayer(event.getGuild().getIdLong());
+        if (player.getPlayer().getPlayingTrack() == null || connectedUsers(audioManager.getConnectedChannel()) == 1)
+        {
+            String name = audioManager.getConnectedChannel().getName();
+            event.getGuild().getAudioManager().closeAudioConnection();
+            event.replyEmbeds(EmbedUtil.defaultEmbed("Disconnected from **"+name+"**!")).queue();
+            return;
+        }
+        if (!ctx.getGuildData().isDJ(event.getMember()))
+        {
+            boolean allowedToStop = true;
+            long userId = event.getUser().getIdLong();
+            List<AudioTrack> tracks = new ArrayList<>(player.getScheduler().getQueue());
+            tracks.add(player.getPlayer().getPlayingTrack());
+            for (AudioTrack track : tracks)
+            {
+                TrackInfo trackInfo = track.getUserData(TrackInfo.class);
+                if (trackInfo.getRequesterIdLong() != userId)
+                {
+                    allowedToStop = false;
+                    break;
+                }
+            }
+            if (!allowedToStop)
+            {
+                event.replyEmbeds(EmbedUtil.errorEmbed("You need to be a DJ to do this!")).queue();
+                return;
+            }
+        }
+
         String name = audioManager.getConnectedChannel().getName();
         event.getGuild().getAudioManager().closeAudioConnection();
         event.replyEmbeds(EmbedUtil.defaultEmbed("Disconnected from **"+name+"**!")).queue();
+    }
+
+    private int connectedUsers(@Nonnull VoiceChannel channel)
+    {
+        int nonBots = 0;
+        for (Member member : channel.getMembers())
+        {
+            if (!member.getUser().isBot())
+                nonBots++;
+        }
+        return nonBots;
     }
 }
