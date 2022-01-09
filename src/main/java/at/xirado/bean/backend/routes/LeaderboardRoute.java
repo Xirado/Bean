fe89b0b0b8752e11566a98ab44d8d4e1deaff21e
Marcel Korzonek <marcel.korzonek@htlstp.at>
2022-01-09 01:37:02 +0100

    Add guild metadata to levels endpoint

--- src/main/java/at/xirado/bean/backend/routes/LeaderboardRoute.java
+++ src/main/java/at/xirado/bean/backend/routes/LeaderboardRoute.java
@@ -1,6 +1,8 @@
 package at.xirado.bean.backend.routes;
 
+import at.xirado.bean.Bean;
 import at.xirado.bean.data.RankingSystem;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.utils.data.DataArray;
 import net.dv8tion.jda.api.utils.data.DataObject;
 import net.dv8tion.jda.internal.utils.Helpers;
@@ -30,6 +32,17 @@ public class LeaderboardRoute implements Route
                     .put("code", 404)
                     .put("message", "The requested data could not be found!").toString();
         }
+        Guild guild = Bean.getInstance().getShardManager().getGuildById(guildIdLong);
+        if (guild == null)
+        {
+            response.status(404);
+            return DataObject.empty()
+                    .put("code", 404)
+                    .put("message", "I am no longer in this guild!").toString();
+        }
+        DataObject data = DataObject.empty()
+                .put("name", guild.getName())
+                .put("icon", guild.getIconUrl());
         String pageHeader = request.queryParams("page");
         int availablePages = (int) Math.ceil((double) totalAmount / 100);
         int page = 1;
@@ -41,6 +54,7 @@ public class LeaderboardRoute implements Route
         }
         DataArray result = RankingSystem.getLeaderboard(guildIdLong, page, 100);
         DataObject obj = DataObject.empty()
+                .put("guild", data)
                 .put("users", result);
         if (availablePages > page)
             obj.put("paging", DataObject.empty().put("next", (request.url().replace("http://", "https://"))+"?page="+(page+1)));
