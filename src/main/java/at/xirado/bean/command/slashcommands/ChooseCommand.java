8fd443ebdb3c5def30048a2100f3fee91a4557df
DJ::Ötzi <maxgabri@icloud.com>
2022-01-12 11:00:52 +0100

    fixed stuff mentioned in PR

--- src/main/java/at/xirado/bean/command/slashcommands/ChooseCommand.java
+++ src/main/java/at/xirado/bean/command/slashcommands/ChooseCommand.java
@@ -10,7 +10,6 @@ import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -18,7 +17,7 @@ public class ChooseCommand extends SlashCommand
 {
     public ChooseCommand()
     {
-        setCommandData(new CommandData("choose", "Chooses between 2 things.")
+        setCommandData(new CommandData("choose", "Chooses between up to 10 things.")
                 .addOption(OptionType.STRING, "1st", "First argument.", true)
                 .addOption(OptionType.STRING, "2nd", "Second argument.", true)
                 .addOption(OptionType.STRING, "3rd", "Third argument.", false)
@@ -38,19 +37,7 @@ public class ChooseCommand extends SlashCommand
     public void executeCommand(@NotNull SlashCommandEvent event, @Nullable Member sender, @NotNull SlashCommandContext ctx)
     {
         List<OptionMapping> chooseOptions = event.getOptions();
-        if(checkOptions(chooseOptions)){
-            ctx.reply("Trying to exploit bots and annoying other people is not nice :(").setEphemeral(true).queue();
-            return;
-        }
         int i = ThreadLocalRandom.current().nextInt(event.getOptions().size());
         ctx.reply(String.format("I choose... %s", chooseOptions.get(i).getAsString())).queue();
-    }
-
-    public boolean checkOptions(List<OptionMapping> options){ //checks if a option could lead to a mass-ping
-        for(OptionMapping option : options){
-            if(option.getAsString().contains("@everyone") || option.getAsString().contains("@here"))
-                return true;
-        }
-        return false;
     }
 }
