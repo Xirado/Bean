e52e5470359e7e95ad10cf733724db1489edd90f
Xirado <bquickzz0@gmail.com>
2022-01-24 20:21:12 +0100

    Remove unnecessary stuff

--- src/main/java/lavaplayer/SpotifyTrack.java
+++ src/main/java/lavaplayer/SpotifyTrack.java
@@ -1,14 +1,7 @@
 package lavaplayer;
 
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException.Severity;
-import com.sedmelluq.discord.lavaplayer.track.AudioItem;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
-import com.sedmelluq.discord.lavaplayer.track.AudioReference;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
-import com.sedmelluq.discord.lavaplayer.track.DelegatedAudioTrack;
-import com.sedmelluq.discord.lavaplayer.track.InternalAudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.*;
 import com.sedmelluq.discord.lavaplayer.track.playback.LocalAudioTrackExecutor;
 import se.michaelthelin.spotify.model_objects.specification.Album;
 import se.michaelthelin.spotify.model_objects.specification.ArtistSimplified;
@@ -55,37 +48,17 @@ public class SpotifyTrack extends DelegatedAudioTrack{
         return this.artworkURL;
     }
 
-    private String getQuery(){
-        var query = "ytsearch:" + trackInfo.title;
-        if(!trackInfo.author.equals("unknown")){
-            query += " " + trackInfo.author;
-        }
-        return query;
-    }
-
     @Override
-    public void process(LocalAudioTrackExecutor executor) throws Exception{
-        AudioItem track = null;
-        if(this.isrc != null){
-            track = this.spotifyAudioSource.loadItem(null, new AudioReference("ytsearch:\"" + this.isrc + "\"", null));
-        }
-        if(track == null){
-            track = this.spotifyAudioSource.loadItem(null, new AudioReference(getQuery(), null));
-        }
-
-        if(track instanceof AudioPlaylist){
-            track = ((AudioPlaylist) track).getTracks().get(0);
-        }
-        if(track instanceof InternalAudioTrack){
-            processDelegate((InternalAudioTrack) track, executor);
-            return;
-        }
-        throw new FriendlyException("No matching Spotify track found", Severity.COMMON, new RuntimeException());
-    }
+    public void process(LocalAudioTrackExecutor executor) {}
 
     @Override
     public AudioSourceManager getSourceManager(){
         return this.spotifyAudioSource;
+    }
+
+    @Override
+    protected AudioTrack makeShallowClone(){
+        return new SpotifyTrack(trackInfo, isrc, artworkURL, this.spotifyAudioSource);
     }
 
 }
