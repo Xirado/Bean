acd54d63a1450ef6136c424dd6b0eb6aa565d791
Damon <damon@montague.im>
2022-01-27 14:44:18 -0500

    Fixed cringe (#14)

--- src/main/java/at/xirado/bean/command/handler/SlashCommandHandler.java
+++ src/main/java/at/xirado/bean/command/handler/SlashCommandHandler.java
@@ -306,7 +306,8 @@ public class SlashCommandHandler
                     command.executeCommand(event, member, ctx);
                 }
 
-            } catch (Exception e)
+            } 
+            catch (Exception e)
             {
                 LinkedDataObject translation = event.getGuild() == null ? LocaleLoader.getForLanguage("en_US") : LocaleLoader.ofGuild(event.getGuild());
                 if (event.isAcknowledged())
