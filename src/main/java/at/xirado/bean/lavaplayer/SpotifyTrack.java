54eaf8c6d9a7973874a1410357e0045e0dcf1880
Xirado <bquickzz0@gmail.com>
2022-02-19 19:36:28 +0100

    Remove spotifyapi dependency

--- src/main/java/at/xirado/bean/lavaplayer/SpotifyTrack.java
+++ src/main/java/at/xirado/bean/lavaplayer/SpotifyTrack.java
@@ -5,7 +5,6 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import com.sedmelluq.discord.lavaplayer.track.DelegatedAudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.playback.LocalAudioTrackExecutor;
-import se.michaelthelin.spotify.model_objects.specification.*;
 
 public class SpotifyTrack extends DelegatedAudioTrack
 {
@@ -14,33 +13,12 @@ public class SpotifyTrack extends DelegatedAudioTrack
     private final String artworkURL;
     private final SpotifyAudioSource spotifyAudioSource;
 
-    public SpotifyTrack(String title, String identifier, String isrc, Image[] images, String uri, ArtistSimplified[] artists, Integer trackDuration, SpotifyAudioSource spotifySourceManager)
-    {
-        this(new AudioTrackInfo(title,
-                artists.length == 0 ? "unknown" : artists[0].getName(),
-                trackDuration.longValue(),
-                identifier,
-                false,
-                "https://open.spotify.com/track/" + identifier
-        ), isrc, images.length == 0 ? null : images[0].getUrl(), spotifySourceManager);
-    }
-
     public SpotifyTrack(AudioTrackInfo trackInfo, String isrc, String artworkURL, SpotifyAudioSource spotifySourceManager)
     {
         super(trackInfo);
         this.isrc = isrc;
         this.artworkURL = artworkURL;
         this.spotifyAudioSource = spotifySourceManager;
-    }
-
-    public static SpotifyTrack of(TrackSimplified track, Album album, SpotifyAudioSource spotifySourceManager)
-    {
-        return new SpotifyTrack(track.getName(), track.getId(), null, album.getImages(), track.getUri(), track.getArtists(), track.getDurationMs(), spotifySourceManager);
-    }
-
-    public static SpotifyTrack of(Track track, SpotifyAudioSource spotifySourceManager)
-    {
-        return new SpotifyTrack(track.getName(), track.getId(), track.getExternalIds().getExternalIds().getOrDefault("isrc", null), track.getAlbum().getImages(), track.getUri(), track.getArtists(), track.getDurationMs(), spotifySourceManager);
     }
 
     public String getISRC()
