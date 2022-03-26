f80e698cbcef659627d159ea25821c9510ff02a8
Xirado <bquickzz0@gmail.com>
2022-03-26 14:30:46 +0100

    Use addRoleToMember() instead of modifyMemberRoles()

--- src/main/java/at/xirado/bean/event/GuildMemberJoinListener.java
+++ src/main/java/at/xirado/bean/event/GuildMemberJoinListener.java
@@ -1,6 +1,5 @@
 package at.xirado.bean.event;
 
-import at.xirado.bean.Bean;
 import at.xirado.bean.data.GuildData;
 import at.xirado.bean.data.GuildManager;
 import at.xirado.bean.data.RankingSystem;
@@ -14,7 +13,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 public class GuildMemberJoinListener extends ListenerAdapter
 {
@@ -29,26 +27,22 @@ public class GuildMemberJoinListener extends ListenerAdapter
 
         GuildData guildData = GuildManager.getGuildData(event.getGuild());
 
-        Bean.getInstance().getScheduledExecutor().schedule(() ->
+        long totalXP = RankingSystem.getTotalXP(event.getGuild().getIdLong(), event.getUser().getIdLong());
+        int level = RankingSystem.getLevel(totalXP);
+        if (level > 0)
         {
-            long totalXP = RankingSystem.getTotalXP(event.getGuild().getIdLong(), event.getUser().getIdLong());
-            int level = RankingSystem.getLevel(totalXP);
-            if (level > 0)
+            List<RoleReward> roleRewards = guildData.getEffectiveRoleRewards(level);
+            Set<Role> rolesToAdd = new HashSet<>();
+            for (RoleReward reward : roleRewards)
             {
-                List<RoleReward> roleRewards = guildData.getEffectiveRoleRewards(level);
-                Set<Role> rolesToAdd = new HashSet<>(event.getMember().getRoles());
-                for (RoleReward reward : roleRewards)
+                if (reward.isPersistant())
                 {
-                    if (reward.isPersistant())
-                    {
-                        Role role = event.getGuild().getRoleById(reward.getRoleId());
-                        if (role != null && event.getGuild().getSelfMember().canInteract(role))
-                            rolesToAdd.add(role);
-                    }
+                    Role role = event.getGuild().getRoleById(reward.getRoleId());
+                    if (role != null && event.getGuild().getSelfMember().canInteract(role))
+                        rolesToAdd.add(role);
                 }
-                if (!rolesToAdd.isEmpty())
-                    event.getGuild().modifyMemberRoles(event.getMember(), rolesToAdd).queue();
             }
-        }, 10, TimeUnit.SECONDS);
+            for (Role role : rolesToAdd)
+                event.getGuild().addRoleToMember(event.getMember(), role).queue(); // Need to do this because otherwise other roles get overwritten
     }
 }
