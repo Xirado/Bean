586b5315fbf8e3c4d484362b6708cccb7f9db6c1
Marcel Korzonek <marcel@xirado.dev>
2025-01-29 18:41:29 +0100

    Implement /flags list

--- src/main/java/at/xirado/bean/interaction/command/slash/dev/UserFlagsCommand.kt
+++ src/main/java/at/xirado/bean/interaction/command/slash/dev/UserFlagsCommand.kt
@@ -7,8 +7,11 @@ import at.xirado.bean.interaction.command.model.slash.Subcommand
 import at.xirado.bean.interaction.command.model.slash.dsl.subcommand.option
 import at.xirado.bean.model.GuildFlag
 import at.xirado.bean.model.UserFlag
+import dev.minn.jda.ktx.messages.Embed
+import dev.minn.jda.ktx.messages.MessageCreate
 import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.utils.messages.MessageCreateData
 import org.koin.core.annotation.Single
 
 @Single
@@ -27,8 +30,28 @@ class UserFlagsCommand(
         }
 
         @Handler
-        suspend fun run(event: SlashCommandInteractionEvent, user: User) {
+        suspend fun run(event: SlashCommandInteractionEvent, user: User): MessageCreateData {
+            val dbUser = userCacheView.save(user)
 
+            return MessageCreate {
+                embeds += Embed {
+                    color = 0x55ff88
+                    author {
+                        name = user.name
+                        iconUrl = user.effectiveAvatarUrl
+                    }
+                    field {
+                        name = "User flags"
+                        value = if (dbUser.flags.isEmpty()) "No flags" else buildString {
+                            dbUser.flags.forEach { flag ->
+                                append(flag.humanName)
+                                append(" (Offset ${flag.offset})")
+                                append('\n')
+                            }
+                        }.trim()
+                    }
+                }
+            }
         }
     }
 
